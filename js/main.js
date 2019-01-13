c0c96d7c0d4ce9928180c9a5de8307b3efdd4786
Harutyun Khangeldyan <khangeldyanh@gmail.com>
2019-01-14 01:57:39 +0400

    carousel added

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$('#top-slider .owl-carousel').owlCarousel({
+    loop:false,
+    dots: true,
+    dotsContainer:'#top-slider-control .dots',
+    dotsClass:'.dot',
+    margin:0,
+    nav:true,
+    navContainer:'#top-slider-control',
+    navText:[$('#top-slider-control .prev'),$('#top-slider-control .next')],
+    items:1
+})
+
+	$('#projects .forx .owl-carousel').owlCarousel({
+    loop:false,
+    dots: true,
+    dotsContainer:'#projects-slider-control .dots',
+    dotsClass:'.dot',
+    margin:0,
+    nav:true,
+    navContainer:'#projects-slider-control',
+    navText:[$('#projects-slider-control .prev'),$('#projects-slider-control .next')],
+    items:1
+})
+
+	$('#project-categories .owl-carousel').owlCarousel({
+    loop:false,
+    dots: false,
+    margin:0,
+    nav:true,
+    navContainer:'#project-categories .pagination',
+    navText:[$('#project-categories .pagination .left'),$('#project-categories .pagination .right')],
+    items:1
+})
+
+	$('#articles .owl-carousel').owlCarousel({
+    loop:false,
+    dots: false,
+    margin:0,
+    nav:true,
+    items:1
+})
+
+});
